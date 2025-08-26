1d872d2510e848e77038a6f402d7ab2028c810ec
charlimoo <141790588+charlimoo@users.noreply.github.com>
2025-08-26 11:39:25 +0330

    345345

--- student_affairs_project/settings.py
+++ student_affairs_project/settings.py
@@ -152,6 +152,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://students.nilva.ir",
+    "https://studentbak.nilva.ir",
 ]
 
 # Celery Configuration (Placeholder for notifications)
